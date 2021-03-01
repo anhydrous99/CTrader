b3a1e30268b5b977f6091eac9fedbd5729fef5c1
Armando Herrera <anhydrous99@gmail.com>
2021-03-01 07:20:32 -0600

    added CPM package manager to get some smaller repositories

---
+++ cmake/glew.cmake
@@ -0,0 +1,5 @@
+CPMAddPackage(
+        NAME GLEW
+        GITHUB_REPOSITORY Perlmint/glew-cmake
+        GIT_TAG glew-cmake-2.2.0
+)
---
+++ cmake/glfw3.cmake
@@ -0,0 +1,9 @@
+CPMAddPackage(
+        NAME GLFW
+        GITHUB_REPOSITORY glfw/glfw
+        GIT_TAG 3.3.3
+        OPTIONS
+        "GLFW_BUILD_TESTS OFF"
+        "GLFW_BUILD_EXAMPLES OFF"
+        "GLFW_BULID_DOCS OFF"
+)
