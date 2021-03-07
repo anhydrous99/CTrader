474324fdb72171519d131fa8df78ebb2a6556c42
Armando Herrera <anhydrous99@gmail.com>
2021-03-07 15:46:33 -0600

    Creation and updates to orderbook created :D

--- src/widgets/OrderBook.cpp
+++ src/widgets/OrderBook.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "OrderBook.h"
+
+OrderBook::OrderBook(libCTrader::Websock *websock) : websock(websock) {
+    websock->on_lvl2_snapshot([&](const libCTrader::LVL2Snapshot &snapshot) {
+        {
+            std::unique_lock lock(bids_mutex);
+            bids.clear();
+            bids = snapshot.bids;
+        }
+        {
+            std::unique_lock lock(asks_mutex);
+            asks.clear();
+            asks = snapshot.asks;
+        }
+    });
+
+    websock->on_lvl2_update([&](const libCTrader::LVL2Update &update) {
+        for (const auto& change : update.changes) {
+            const std::string& size = std::get<2>(change);
+            if (std::get<0>(change) == "buy") {
+                if (size == "0") {
+                    std::unique_lock lock(bids_mutex);
+                    bids.erase(std::get<1>(change));
+                } else {
+                    std::unique_lock lock(bids_mutex);
+                    bids[std::get<1>(change)] = size;
+                }
+            } else {
+                if (size == "0") {
+                    std::unique_lock lock(asks_mutex);
+                    asks.erase(std::get<1>(change));
+                } else {
+                    std::unique_lock lock(asks_mutex);
+                    asks[std::get<1>(change)] = size;
+                }
+            }
+        }
+    });
+}
--- src/widgets/OrderBook.h
+++ src/widgets/OrderBook.h
@@ -5,9 +5,17 @@
 #ifndef CTRADER_ORDERBOOK_H
 #define CTRADER_ORDERBOOK_H
 
+#include <libCTrader/Websock.h>
 
 class OrderBook {
+    libCTrader::Websock *websock;
+    std::map<std::string, std::string> bids;
+    std::map<std::string, std::string> asks;
+    std::shared_mutex bids_mutex;
+    std::shared_mutex asks_mutex;
 
+public:
+    explicit OrderBook(libCTrader::Websock *websock);
 };
 
 
