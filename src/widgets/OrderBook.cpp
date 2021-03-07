8a1db0eb3177e74500c0a148b747236918b7ce19
Armando Herrera <anhydrous99@gmail.com>
2021-03-07 07:02:17 -0600

    Started to build order book

---
+++ src/widgets/OrderBook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Armando Herrera on 3/7/21.
+//
+
+#include "OrderBook.h"
---
+++ src/widgets/OrderBook.h
@@ -0,0 +1,14 @@
+//
+// Created by Armando Herrera on 3/7/21.
+//
+
+#ifndef CTRADER_ORDERBOOK_H
+#define CTRADER_ORDERBOOK_H
+
+
+class OrderBook {
+
+};
+
+
+#endif //CTRADER_ORDERBOOK_H
