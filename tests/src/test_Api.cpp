a50f3dfb014aa2f196a9d9506774427416c1ca96
Armando Herrera <anhydrous99@gmail.com>
2021-04-05 16:30:33 -0500

    Created accounts test, setting how the rest of the api test are going to be built

--- tests/src/test_Api.cpp
+++ tests/src/test_Api.cpp
@@ -11,12 +11,12 @@ namespace {
     using ::testing::Values;
 
     struct ApiTest : public ::testing::Test {
-        libCTrader::Client *client;
+        MockClient *client;
         libCTrader::Api *api;
     protected:
         void SetUp() override {
-            client = reinterpret_cast<libCTrader::Client *>(new MockClient());
-            api = new libCTrader::Api(client);
+            client = new MockClient();
+            api = new libCTrader::Api(reinterpret_cast<libCTrader::Client*>(client));
         }
 
         void TearDown() override {
@@ -25,8 +25,32 @@ namespace {
         }
     };
 
-    TEST_F(ApiTest, accounts) {
-
+    TEST_F(ApiTest, accounts) { // NOLINT
+        EXPECT_CALL(*client, call("GET", true, "/accounts")).WillRepeatedly(::testing::Return(R"(
+[
+    {
+        "id": "71452118-efc7-4cc4-8780-a5e22d4baa53",
+        "currency": "BTC",
+        "balance": "0.0000000000000000",
+        "available": "0.0000000000000000",
+        "hold": "0.0000000000000000",
+        "profile_id": "75da88c5-05bf-4f54-bc85-5c775bd68254",
+        "trading_enabled": true
+    },
+    {
+        "id": "e316cb9a-0808-4fd7-8914-97829c1925de",
+        "currency": "USD",
+        "balance": "80.2301373066930000",
+        "available": "79.2266348066930000",
+        "hold": "1.0035025000000000",
+        "profile_id": "75da88c5-05bf-4f54-bc85-5c775bd68254",
+        "trading_enabled": true
+    }
+])"));
+        auto accounts = api->accounts();
+        EXPECT_EQ(accounts.size(), 2);
+        EXPECT_EQ(accounts[0].id, "71452118-efc7-4cc4-8780-a5e22d4baa53");
+        EXPECT_EQ(accounts[1].id, "e316cb9a-0808-4fd7-8914-97829c1925de");
     }
 
     TEST_F(ApiTest, account) {
